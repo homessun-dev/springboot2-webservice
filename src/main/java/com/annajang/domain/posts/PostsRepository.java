7609efc9cca74fbbcc032b3a58100358f1cb6ac5
jangsunhee <hijangssun@gmail.com>
2021-05-26 15:07:37 +0900

    전체 조회 화면 만들기

--- src/main/java/com/annajang/domain/posts/PostsRepository.java
+++ src/main/java/com/annajang/domain/posts/PostsRepository.java
@@ -1,8 +1,14 @@
 package com.annajang.domain.posts;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Query("SELECT p FROM Posts p ORDER BY p.id DESC")
+    List<Posts> findAllDesc();
 }
 
 
@@ -14,4 +20,9 @@ public interface PostsRepository extends JpaRepository<Posts, Long> {
     Entity 클래스와 기본 Entity Repository는 함께 위치해야한다는 점이 중요!!!
     Entity 클래스는 기본 Repository 없이는 제대로 역할을 할 수 없다.
     프로젝트 규모가 커져 도메인별로 프로젝트를 분리해야 한다면 함께 도메인 패키지에서 관리하는게 좋다.
+
+    조회의 경우 FK의 조인, 복잡한 조건으로 Entity클래스 만으로 처리하기 어려워 조회용 프레임워크를 추가로 사용함.
+    대표적예로 querydsl, jooq, MyBatis 등이 있음. 3가지 중에 하나를 통해 조회하고 등록/수정/삭제 등은 SpringDataJpa를 통해 진행. querydsl 추천!!
+    1. 타입안정성 보장 / 2. 국내 많은 회사에서 사용중 / 3. 레퍼런스가 많음(국내 자료가 많아서 질의응답 가능이 큰 장점)
+
  */
--- src/main/java/com/annajang/service/PostsService.java
+++ src/main/java/com/annajang/service/PostsService.java
@@ -2,12 +2,16 @@ package com.annajang.service;
 
 import com.annajang.domain.posts.Posts;
 import com.annajang.domain.posts.PostsRepository;
+import com.annajang.web.dto.PostsListResponseDto;
 import com.annajang.web.dto.PostsResponseDto;
 import com.annajang.web.dto.PostsSaveRequestDto;
 import com.annajang.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RequiredArgsConstructor
 @Service
@@ -33,4 +37,11 @@ public class PostsService {
         return postsRepository.save(requestDto.toEntity()).getId();
     }
 
+
+    @Transactional(readOnly = true) //트랜잭션 범위는 유지, 조회기능만 남겨두어 조회속도 개선.
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new) //람다식. .map(posts -> new PostsListResponseDto(posts)) / Posts의 Stream을 map을 통해 PostsListResponseDto 변환 -> List로 반환하는 메소드.
+                .collect(Collectors.toList());
+    }
 }
